9f80814906ee5dcb05dc91574d61ba3c321e406a
Samira Stivan <elianasamira.stivan@mail.polimi.it>
2020-03-28 15:30:25 +0100

    Change localhost to dwarf7

--- PythonAPI/examples/synchronous_mode.py
+++ PythonAPI/examples/synchronous_mode.py
@@ -129,7 +129,7 @@ def main():
     font = get_font()
     clock = pygame.time.Clock()
 
-    client = carla.Client('localhost', 2000)
+    client = carla.Client('dwarf7.local.necst.it', 2000)
     client.set_timeout(2.0)
 
     world = client.get_world()
